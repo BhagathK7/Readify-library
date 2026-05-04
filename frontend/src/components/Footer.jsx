5fe8dcd6c4a672fb76f9f184c39ee5b984e0d2f6
BhagathK7 <bhagathkrishnasunilkumar.23cse@kongu.edu>
2026-05-04 20:16:29 +0530

    Updated Footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -4,8 +4,8 @@ import React from 'react';
 export default function Footer() {
   return (
     <footer className="footer">
-      <div>© {new Date().getFullYear()} Readify</div>
-      <div className="muted"> Library bookstore</div>
+      <div>© {new Date().getFullYear()} Readify Book Store</div>
+      <div className="muted"> Library Bookstore</div>
     </footer>
   );
 }
