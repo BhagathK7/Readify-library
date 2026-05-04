468c8914a18e4c5969f5ff44a2e5416a8013e558
BhagathK7 <bhagathkrishnasunilkumar.23cse@kongu.edu>
2026-05-04 20:11:55 +0530

    Updated Footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -5,7 +5,7 @@ export default function Footer() {
   return (
     <footer className="footer">
       <div>© {new Date().getFullYear()} Readify</div>
-      <div className="muted">A minimal digital library & bookstore</div>
+      <div className="muted"> Library bookstore</div>
     </footer>
   );
 }
